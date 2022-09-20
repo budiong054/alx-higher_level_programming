e21d80e49d3cdbc4e534563e851b1876c04fb0ba
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-09-20 20:04:50 +0000

    Fix pycodesytle error message

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for alpha in range(97, 123):
     if (alpha == 101) or (alpha == 113):
         continue
-    print("{:c}".format(alpha), end = "")
+    print("{:c}".format(alpha), end="")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for i in range(0, 10):
         if (i == 8) and (j == 9):
             print("{:d}{:d}".format(i, j))
             break
-        print("{:d}{:d}".format(i, j), end = ", ")
+        print("{:d}{:d}".format(i, j), end=", ")
