d6a821998c746228550101c8c4f0d6d9ebfcdec4
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-09-22 15:38:49 +0000

    Fix bugs

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import hidden_4
 
-for name in dir(hidden_4):
-    if name.startswith('_'):
-        continue
-    print(name)
+if __name__ == '__main__':
+    for name in dir(hidden_4):
+        if name.startswith('_'):
+            continue
+        print(name)
