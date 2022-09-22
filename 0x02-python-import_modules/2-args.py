6ef677c73a0eb121c3b114fcda8bb9d99ff3b667
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-09-22 11:49:31 +0000

    Fix bugs in task 2 and 5

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import sys
-length = len(sys.argv)
 
-if length < 2:
-    print(f"{length - 1:d} arguments.")
-else:
-    if length == 2:
-        print(f"{length - 1:d} argument:")
+if __name__ == '__main__':
+    length = len(sys.argv)
+    if length < 2:
+        print(f"{length - 1:d} arguments.")
     else:
-        print(f"{length - 1:d} arguments:")
-    for i in range(1, length):
-        print(f"{i:d}: {sys.argv[i]}")
+        if length == 2:
+            print(f"{length - 1:d} argument:")
+        else:
+            print(f"{length - 1:d} arguments:")
+        for i in range(1, length):
+            print(f"{i:d}: {sys.argv[i]}")
