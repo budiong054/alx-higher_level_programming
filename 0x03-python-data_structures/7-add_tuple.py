3a639e787be736b7535a1409a8b9dac9b419a833
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-09-25 20:41:16 +0000

    Add solution to task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,5 +9,20 @@ def add_tuple(tuple_a=(), tuple_b=()):
     Returns:
         The result of the addition as a tuple
     """
-    if len(tuple_a) < 2 or len(tuple_b) < 2:
-        pass
+
+    tuple_list = []
+    i = 0
+    while i < 2:
+        if i > len(tuple_a) - 1:
+            a = 0
+        else:
+            a = tuple_a[i]
+
+        if i > len(tuple_b) - 1:
+            b = 0
+        else:
+            b = tuple_b[i]
+        tuple_list.append(a + b)
+        i += 1
+
+    return tuple(tuple_list)
