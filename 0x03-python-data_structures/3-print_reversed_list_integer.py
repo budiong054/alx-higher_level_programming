213d695091039576deb606ddf03585f64ba6394d
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-09-27 06:59:08 +0000

    Fix PEP8 error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -9,7 +9,7 @@ def print_reversed_list_integer(my_list=[]):
     Returns:
         None
     """
-    if my_list == None:
+    if my_list is None:
         return
     list_len = len(my_list)
     for i in range(1, list_len + 1):
