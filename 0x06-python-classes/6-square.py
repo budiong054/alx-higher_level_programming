0703b158d02a0e87070c965a7e1f956d89a5acd9
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-10-11 22:37:57 +0000

    Fix bugs

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -81,5 +81,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
+            for i in range(self.__position[1]):
+                print()
             for i in range(self.__size):
                 print(f'{self.__position[0] * " "}{self.__size * "#"}')
