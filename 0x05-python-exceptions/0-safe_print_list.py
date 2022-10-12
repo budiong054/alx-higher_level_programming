b7a0b2e07534303d6f04b2600fa18873dc2a9d43
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-10-12 17:29:25 +0000

    Add solution to task 5 and 6

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,6 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     try:
         if x == 0:
+            print()
             return 0
         for i in range(x):
             print("{}".format(my_list[i]), end="")
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     if x == 0:
+        print()
         return 0
     n = 0
     for i in range(x):
