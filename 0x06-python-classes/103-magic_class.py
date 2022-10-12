88be7988b5fc46df0ee174a1b19557df7dfa5864
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-10-12 22:41:51 +0000

    Fix bug

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -6,7 +6,7 @@ import math
 
 class MagicClass:
     """magicClass calculates the area and the circumference of a circle"""
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
