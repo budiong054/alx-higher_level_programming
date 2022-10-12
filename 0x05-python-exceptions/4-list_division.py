88c8860fed5b843ba97a9aa04a1f4f6e5ba02931
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-10-12 21:27:57 +0000

    Fix bugs in task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -12,8 +12,8 @@ def list_division(my_list_1, my_list_2, list_length):
             except (TypeError, ValueError):
                 print("wrong type")
                 new_list.append(0)
-    except IndexError:
-        print("out of range")
-        new_list.append(0)
+            except IndexError:
+                print("out of range")
+                new_list.append(0)
     finally:
         return new_list
