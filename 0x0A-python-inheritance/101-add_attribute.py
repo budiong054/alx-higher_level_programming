fab670c30941091f45eaad0992eccbbede776111
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-10-25 00:46:58 +0000

    Add a partial solution to task 101

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""The ``101-add_attribute`` module
+
+This module contains one function
+"""
+
+
+def add_attribute(obj, name, value):
+    """Adds a new attribute to an object if it's possible
+    """
+    obj.name = value
