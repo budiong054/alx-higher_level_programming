4cf1f45cbe8e6181f76152936872f210df280fee
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-10-26 04:53:57 +0000

    Add solution to task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""The ``11-student`` module
+
+This module contains a ``Student`` class
+"""
+
+
+class Student:
+    """This is a students class
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionaru representation of a
+            `Student` instance
+        """
+        if attrs is not None:
+            new_dict = dict()
+            for key in attrs:
+                if self.__dict__.get(key) is not None:
+                    new_dict[key] = self.__dict__.get(key)
+            return new_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the `Student` instance
+        """
+        self.first_name = json['first_name']
+        self.last_name = jason['last_name']
+        self.age = json['age']
