c4ff8a6cb75b59ae00cf584606f9ab972333c4fe
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-12-19 18:28:50 +0000

    Fix semistandard error

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,11 +3,11 @@
 
 const num = parseInt(process.argv[2]);
 
-function factorial(num) {
-	if (isNaN(num) || num <= 1) {
-		return (1);
-	}
-	return (num * factorial(num - 1));
+function factorial (num) {
+  if (isNaN(num) || num <= 1) {
+    return (1);
+  }
+  return (num * factorial(num - 1));
 }
 
 console.log(factorial(num));
