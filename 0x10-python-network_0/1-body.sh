4435c63ae2f763ba3329a82fc90e51b1513e72d6
Blessing Udiong <udiongblessing@gmail.com>
2023-02-07 12:45:15 +0000

    Replace /usr/bin/env bash

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # This script takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -sL -w "%{http_code}" $1 -o /dev/null | grep -q "200" && curl -sL $1
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # This script takes in a URL and displays all HTTP methods the server will accept.
 curl -sIX OPTIONS $1 | grep "Allow" | cut -d " " -f 2
