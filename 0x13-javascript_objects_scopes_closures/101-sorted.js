99ed0c5188d45303de607202d080625bade1dd1a
Blessing Udiong <udiongblessing@gmail.com>
2023-03-15 10:45:26 +0000

    Fix semistandard error

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,18 +3,18 @@ const dict = require('./101-data').dict;
 
 // Get all the value in an  array
 const newArray = [];
-for (let key in dict){
-	newArray.push(dict[key]);
+for (const key in dict) {
+  newArray.push(dict[key]);
 }
 
-//Create a new dictionary with the unique value in the newArray
+// Create a new dictionary with the unique value in the newArray
 const newDict = {};
-for (let element of [...new Set(newArray)]){
-	newDict[element] = [];
+for (const element of [...new Set(newArray)]) {
+  newDict[element] = [];
 }
 
 // Push the keys to the new dictionary with corresponding value
-for (let key in dict){
-	newDict[dict[key]].push(key);
+for (const key in dict) {
+  newDict[dict[key]].push(key);
 }
 console.log(newDict);
