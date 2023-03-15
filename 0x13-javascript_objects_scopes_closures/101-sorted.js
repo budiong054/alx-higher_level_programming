5f148e7897dd4ad19632cb9598efbe2e6a0e0d7d
Blessing Udiong <udiongblessing@gmail.com>
2023-03-15 10:34:38 +0000

    Add sort and concat files

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,4 +1,20 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
-const newDict = dict;
+
+// Get all the value in an  array
+const newArray = [];
+for (let key in dict){
+	newArray.push(dict[key]);
+}
+
+//Create a new dictionary with the unique value in the newArray
+const newDict = {};
+for (let element of [...new Set(newArray)]){
+	newDict[element] = [];
+}
+
+// Push the keys to the new dictionary with corresponding value
+for (let key in dict){
+	newDict[dict[key]].push(key);
+}
 console.log(newDict);
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,18 @@
 #!/usr/bin/node
-const { FilesManager } = require('turbodepot-node');
-const filesManager = new FilesManager();
-const argv = process.argv;
-filesManager.mergeFiles([argv[2], argv[3]], argv[4], '\n');
+
+const fs = require('fs');
+
+// Receiving the file from the command line
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+// Reading the content of the file
+const dataA = fs.readFileSync(fileA, 'utf8');
+const dataB = fs.readFileSync(fileB, 'utf8');
+
+// Concatinating the data
+const dataC = dataA + dataB;
+
+// writing the new data into the destination file
+fs.writeFileSync(fileC, dataC);
