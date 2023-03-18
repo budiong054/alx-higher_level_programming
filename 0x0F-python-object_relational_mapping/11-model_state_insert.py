f5c95e99ec3d01ead0b8d0c5ed8ef63c72de1865
Blessing Udiong <udiongblessing@gmail.com>
2023-03-18 08:38:08 +0000

    Rename file

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -13,8 +13,6 @@ if __name__ == '__main__':
 
     engine = create_engine(f"mysql+mysqldb://{user}:{passwd}@localhost/{db}")
 
-    Base.metadata.create_all(engine)
-    
     Session = sessionmaker(bind=engine)
     session = Session()
 
