51f2db5dbdab76f2c89c1c53ee2457bc25ad19eb
Blessing Udiong <udiongblessing@gmail.com>
2023-03-31 21:03:33 +0000

    Modify script to send a Post request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a POST request to the passed URL, and displays the body of the response
-curl -sX POST "$1" -d "email: test@gmail.com" -d "subject: I will always be here for PLD"
+curl -sX POST "$1" -d "email=test@gmail.com&subject=I will always be here for PLD"
